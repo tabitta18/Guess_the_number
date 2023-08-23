a51118209666629581e5e6cefe4d2d8ebc57a2d4
Tabitha Cat <134290478+tabitta18@users.noreply.github.com>
2023-08-23 18:45:48 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import random
+import eel
+
+# Инициализация Eel
+eel.init('web')
+
+# Генерация случайного числа
+number_to_guess = random.randint(1, 100)
+
+# Функция для проверки угаданного числа
+@eel.expose
+def check_guess(guess):
+    guess = int(guess)
+    if guess < number_to_guess:
+        return "Мало!"
+    elif guess > number_to_guess:
+        return "Много!"
+    else:
+        return "Верно!"
+
+# Запуск Eel с веб-интерфейсом
+eel.start('index.html', size=(400, 200))
