5dc94f183da3d916df125581e40e2102bdf5dc6d
Tabitha Cat <134290478+tabitta18@users.noreply.github.com>
2023-09-01 17:22:16 +0500

    Update main2.py

--- main2.py
+++ main2.py
@@ -4,7 +4,7 @@ def guess_number():
     secret_number = random.randint(1, 100)
     attempts = 0
 
-    print("Привет! Давай сыграем в игру 'Угадай число'.")
+    print("Давай сыграем в игру 'Угадай число'.")
 
     while True:
         guess = int(input("Угадай число от 1 до 50: "))
